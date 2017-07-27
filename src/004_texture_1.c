fc802a76aad43b3a969617585781119d82aa693b
dmx <dingmingxin20@gmail.com>
2017-07-27 15:50:38 +0800

    Initial commit

---
+++ src/004_texture_1.c
@@ -0,0 +1,228 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+const GLuint WIN_WIDTH = 800, WIN_HEIGHT = 600;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+//shaders
+
+const GLchar* vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos; \n"
+	"layout (location = 1) in vec3 aColor; \n"
+	"layout (location = 2) in vec2 aTexCoord; \n"
+	"out vec3 ourColor; \n"
+	"out vec2 TexCoord; \n"
+	"void main()\n"
+	"{\n"
+	"gl_Position = vec4(aPos, 1.0); \n"
+	"ourColor = aColor;\n"
+	"TexCoord = aTexCoord;\n"
+	"}\0";
+
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"in vec3 ourColor;\n"
+	"in vec2 TexCoord;\n"
+	"uniform sampler2D ourTexture;\n"
+	"void main()\n"
+	"{\n"
+	"//FragColor = texture(ourTexture, TexCoord);\n" //origin textures
+	"FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);\n" //get fancy colors on texture
+	"}\n\0";
+
+void set_tex_parameters()
+{
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+}
+
+void bind_vetex_attributes(GLuint *pVAO, GLuint *pVBO, GLuint *pEBO)
+{
+    // Set up vertex data (and buffer(s)) and attribute pointers
+    GLfloat vertices[] = {
+    	//position        //color             //texture coords
+         0.5f,   0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,// top right  
+         0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,// bottom right 
+         -0.5f,  -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,// bottom left   
+         -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,// top left
+    };
+
+	unsigned int indices[] = {  
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
+	};
+
+    glGenVertexArrays(1, pVAO);
+    glGenBuffers(1, pVBO);
+    glGenBuffers(1, pEBO);
+    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+    glBindVertexArray(*pVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, *pVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *pEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    int stride_size = 8;
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride_size * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride_size * sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride_size * sizeof(GLfloat), (GLvoid*)(6*sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
+}
+void load_image_data()
+{
+
+    int width, height, nrChannels;
+    unsigned char * data = stbi_load("res/wall.jpg", &width, &height, &nrChannels, 0);
+    if(data) 
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else 
+    {
+        printf("Failed to load texture\n");
+    }
+    stbi_image_free(data);
+}
+
+int 
+main()
+{
+	//init glfw
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+#endif
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+
+	//create a glfwwindow object that we can use for glfw`s functions
+	GLFWwindow* window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "LearnOpenGL", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+
+	//set the require callback functions
+	glfwSetKeyCallback(window, key_callback);
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	//viewport
+	int viewPortWidth, viewPortHeight;
+	glfwGetFramebufferSize(window, &viewPortWidth, &viewPortHeight);
+	glViewport(0, 0, viewPortWidth, viewPortHeight);
+
+	// build and compile our shaders program
+	// Vertex shader 顶点着色器
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// check for compile time errors
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("ERROR:shader: vertex compile failed:%s\n", infoLog);
+	}
+
+    // Fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // Check for compile time errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED %s \n", infoLog);
+    }
+    // Link shaders
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // Check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        printf("ERROR::SHADER::PROGRAM::LINKING_FAILED %s \n", infoLog);
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	set_tex_parameters();
+    load_image_data();
+
+    GLuint VBO, VAO, EBO;
+    bind_vetex_attributes(&VAO, &VBO, &EBO);
+
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+
+    glBindVertexArray(0); // Unbind VAO 
+
+    // Game loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Render
+        // Clear the colorbuffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+		//bind texture
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        glUseProgram(shaderProgram);
+
+        glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        // Swap the screen buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    // Terminate GLFW, clearing any resources allocated by GLFW.
+    glfwTerminate();
+    return 0;
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
