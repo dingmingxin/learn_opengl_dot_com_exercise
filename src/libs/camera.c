ac5032d84fc851e7b491cdb92845c1e1161be344
dmx <dingmingxin20@gmail.com>
2017-10-31 19:29:41 +0800

    add camera

---
+++ src/libs/camera.c
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  camera.c
+ *
+ *    Description:  opengl camera class implemented in c
+ *
+ *        Version:  1.0
+ *        Created:  10/30/2017 10:42:02
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  dingmingxin (dmx), dingmingxin20@gmail.com
+ *   Organization:  coding
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+#define CAMERA_DAFULT_YAW -90.0f
+#define CAMERA_PITCH -90.0f
---
+++ src/libs/camera.h
@@ -0,0 +1,51 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  camera.h
+ *
+ *    Description:  opengl camera class implemented in c
+ *
+ *        Version:  1.0
+ *        Created:  10/30/2017 10:41:40
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  dingmingxin (dmx), dingmingxin20@gmail.com
+ *   Organization:  coding
+ *
+ * =====================================================================================
+ */
+
+#ifndef  camera_INC
+#define  camera_INC
+
+enum CAMERA_MOVEMENT {
+	CM_FORWARD,
+	CM_BACKWARD,
+	CM_LEFT,
+	CM_RIGHT
+};
+
+struct camera {
+	//Movement
+	float move_speed;
+	float move_sensitivity;
+	float zoom;
+
+	//Eular Angles
+	float pitch;
+	float yaw;
+	float roll;
+
+	//Positions
+	float pos[3];
+	float front[3];
+	float up[3];
+	float right[3];
+	float worldup[3];
+};
+
+struct camera * camera_init(struct camera *camera);
+void camera_move(CAMERA_MOVEMENT move_type, float offset)
+
+#endif   /* ----- #ifndef camera_INC  ----- */
