09d9e4fc8381036b6d7df9285f47f5da6b33426c
dmx <dingmingxin20@gmail.com>
2017-11-23 16:31:44 +0800

    2.1.lighting/1.1.colors remove some functions

--- src/2.lighting/1.1.colors/colors.c
+++ src/2.lighting/1.1.colors/colors.c
@@ -70,103 +70,6 @@ void set_tex_parameters()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 }
 
-void bind_vetex_attributes(GLuint *pVAO, GLuint *pVBO)
-{
-    // Set up vertex data (and buffer(s)) and attribute pointers
-    GLfloat vertices[] = {
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-    };
-
-
-    glGenVertexArrays(1, pVAO);
-    glGenBuffers(1, pVBO);
-    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-    glBindVertexArray(*pVAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, *pVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-
-    int stride_size = 5;
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride_size * sizeof(GLfloat), (GLvoid*)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride_size * sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
-}
-
-void load_image_bind_textures(unsigned int *textures)
-{
-
-	stbi_set_flip_vertically_on_load(1); 
-	char *images[2] = {"res/wall.jpg", "res/awesomeface.png"};
-	for(int i=0; i<2 ; i++) {
-		glGenTextures(1, textures+i);
-		glBindTexture(GL_TEXTURE_2D, textures[i]);
-
-		set_tex_parameters();
-		int width, height, nrChannels;
-		unsigned char * data = stbi_load(images[i], &width, &height, &nrChannels, 0);
-		if(data) 
-		{
-			if(i==0) 
-			{
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-			}
-			else 
-			{
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-			}
-			glGenerateMipmap(GL_TEXTURE_2D);
-		}
-		else 
-		{
-			printf("Failed to load texture\n");
-		}
-		stbi_image_free(data);
-	}
-}
-
 struct camera camera;
 
 int 
@@ -192,7 +95,6 @@ main()
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
 
-
 	//set the require callback functions
 	glfwSetKeyCallback(window, key_callback);
 
@@ -206,29 +108,24 @@ main()
 	shader_source("1.1.colors.vs", vertexShaderSource, SHADER_SOURCE_SIZE);
 	shader_create(&shader, fragmentShaderSource, vertexShaderSource);
 
-    GLuint VBO, VAO;
-    bind_vetex_attributes(&VAO, &VBO);
+    GLuint VBO, lightVAO;
+    glGenVertexArrays(1, &lightVAO);
+    glBindVertexArray(lightVAO)
 
-	int textures_len = 2;
-	unsigned int textures[2] = {0, 0};
-	load_image_bind_textures(textures);
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    
+
 
 	shader_use(&shader);
-
-	glUniform1i(glGetUniformLocation(shader.id, "texture1"), 0);
-	glUniform1i(glGetUniformLocation(shader.id, "texture2"), 1);
 
 	float cubePositions[10][3] = {
 		{0.0f,  0.0f,  0.0f}, 
 		{2.0f,  5.0f, -15.0f}, 
-		{1.5f, -2.2f, -2.5f},  
-		{3.8f, -2.0f, -12.3f},  
-		{-0.4f, -0.4f, -3.5f},  
-		{-1.7f,  3.0f, -7.5f},  
-		{-1.3f, -2.0f, -2.5f},  
-		{-1.5f,  2.0f, -2.5f}, 
-		{1.5f,  0.2f, -1.5f}, 
-		{-1.3f,  1.0f, -1.5f}  
 	};
 
 	glEnable(GL_DEPTH_TEST);
@@ -266,24 +163,8 @@ main()
 		glUniformMatrix4fv(glGetUniformLocation(shader.id, "projection"), 1, GL_FALSE, &projection.mat[0]);
 		glUniformMatrix4fv(glGetUniformLocation(shader.id, "view"), 1, GL_FALSE, &view.mat[0]);
 
-        glBindVertexArray(VAO);
-		for(unsigned int i = 0; i < 10; i++)
-		{
-
-			float angle = 1.0f;
-			kmMat4 model, tmp;
-			kmMat4Identity(&model);
-			kmMat4Translation(&tmp, cubePositions[i][0], cubePositions[i][1], cubePositions[i][2]);
-			kmMat4Multiply(&model, &model, &tmp);
-
-			kmMat4Identity(&tmp);
-			kmMat4RotationYawPitchRoll(&tmp, 1.0f*angle, 0.6f*angle, 0.5f*angle);
-			kmMat4Multiply(&model, &model, &tmp);
-
-			glUniformMatrix4fv(glGetUniformLocation(shader.id, "model"), 1, GL_FALSE, &model.mat[0]);
-
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
+        glBindVertexArray(lightVAO);
+//		glDrawArrays(GL_TRIANGLES, 0, 36);
 
         glBindVertexArray(0);
 
