63e42a913fe80ea8aa1c2f90384318b16b1767b3
dmx <dingmingxin20@gmail.com>
2018-08-29 14:54:49 +0800

    update

--- src/1.getting_started/1.1.hello_window/hello_window.c
+++ src/1.getting_started/1.1.hello_window/hello_window.c
@@ -5,10 +5,11 @@
 //glfw
 #include <GLFW/glfw3.h>
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void
+key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
 
-int main(int argc, char *argv[])
-{
+int
+main(int argc, char *argv[]) {
 	if (!glfwInit()) {
 		fprintf(stderr, "Failed to initialize GLFW\n");
 		return -1;
@@ -21,30 +22,24 @@ int main(int argc, char *argv[])
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-	if (window == NULL)
-	{
+	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
 		printf("Failed to create GLFW window");
 		glfwTerminate();
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
-		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-			return -1;
-		}
-
-		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-			return -1;
-		}
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		return -1;
+	}
 
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
 
 	glViewport(0, 0, width, height);
 
-	glfwSetKeyCallback(window, key_callback);  
-	while(!glfwWindowShouldClose(window))
-	{
+	glfwSetKeyCallback(window, key_callback);
+	while(!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 
 		glfwSwapBuffers(window);
@@ -54,10 +49,11 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-{
-    // When a user presses the escape key, we set the WindowShouldClose property to true, 
-    // closing the application
-    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-    	glfwSetWindowShouldClose(window, GL_TRUE);
-}  
+void
+key_callback(GLFWwindow *window, int key, int scancode, int action, int mode) {
+	// When a user presses the escape key, we set the WindowShouldClose property to true,
+	// closing the application
+	if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
